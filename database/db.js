c4d827e1bac99d76187dd8845d642ca61f1b798e
Kumar Gaurav <kumardeveloper80@gmail.com>
2025-01-07 15:30:51 +0530

    done

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoString = "mongodb+srv://vivekanjankc:vivek@cluster0.czyye.mongodb.net/";
+const mongoString = "mongodb+srv://vivekanjankc:vivek@cluster0.czyye.mongodb.net";
 
 exports.connectDB = async () => {
   try {
