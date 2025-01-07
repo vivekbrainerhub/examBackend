092c9a1a66fc681a49a8b89190c5af8d2b5d2231
Kumar Gaurav <kumardeveloper80@gmail.com>
2025-01-07 15:38:04 +0530

    done

--- database/db.js
+++ database/db.js
@@ -1,12 +1,40 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+// const mongoString = "mongodb+srv://vivekanjankc:vivek@cluster0.czyye.mongodb.net";
+
+// exports.connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(mongoString);
+//     console.log("Mongoose DB Connected");
+//   } catch (error) {
+//     // console.log("Mongoose DB not Connected");
+//     process.exit(1);
+//   }
+// };
+const mongoose = require('mongoose');
+
 const mongoString = "mongodb+srv://vivekanjankc:vivek@cluster0.czyye.mongodb.net";
 
 exports.connectDB = async () => {
   try {
-    const conn = await mongoose.connect(mongoString);
+    // MongoDB connection options
+    const mongooseOptions = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, 
+    };
+
+    // Establish connection to the MongoDB database
+    const conn = await mongoose.connect(mongoString, mongooseOptions);
     console.log("Mongoose DB Connected");
   } catch (error) {
-    // console.log("Mongoose DB not Connected");
-    process.exit(1);
+    console.error("Mongoose DB not connected", error.message);
+    process.exit(1);  // Exit the process if there's an error
   }
 };
+
+// Graceful shutdown for when the app is terminated (useful for Vercel)
+process.on('SIGINT', async () => {
+  console.log("Closing MongoDB connection...");
+  await mongoose.connection.close();
+  process.exit(0);  // Exit gracefully
+});
